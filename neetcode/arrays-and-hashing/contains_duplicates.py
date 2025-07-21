feb90f0ea30cf7aa28b6cdd33c87a65b482def63
Linus Juni <linus@juni.dk>
2025-07-21 11:11:14 +0200

    include description

--- neetcode/arrays-and-hashing/contains_duplicates.py
+++ neetcode/arrays-and-hashing/contains_duplicates.py
@@ -1,10 +1,16 @@
+"""
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+"""
+
+from typing import List
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = []
+        seen = set()
 
         for num in nums:
             if num in seen:
                 return True
-            seen.append(num)
+            seen.add(num)
 
         return False
