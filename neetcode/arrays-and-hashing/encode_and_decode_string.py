c76cafb8b12dda5b94f28d21a41596b251d5ba96
Linus Juni <linus@juni.dk>
2025-07-22 13:18:10 +0200

    solve problems

---
+++ neetcode/arrays-and-hashing/encode_and_decode_string.py
@@ -0,0 +1,48 @@
+"""
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+Please implement encode and decode
+"""
+
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        if len(strs) == 0:
+            return ''
+
+        all_strings = ''
+        lengths = []
+        for s in strs:
+            lengths.append(len(s))
+            all_strings += s
+
+        lengths_str = ''
+        for length in lengths:
+            lengths_str += str(length) + ','
+
+        return all_strings + ' | ' + lengths_str[:-1]
+
+    def decode(self, s: str) -> List[str]:
+
+        if s == '':
+            return []
+
+        parts = s.split(' | ', 1)
+        all_strings = parts[0]
+        lengths_string = parts[1]
+
+        lengths = lengths_string.split(',')
+
+        decoded = []
+        i = 0
+
+        for length in lengths:
+            
+            to_append = all_strings[i:int(length)+i]
+            i += int(length)
+            decoded.append(to_append)
+
+        return decoded
---
+++ neetcode/arrays-and-hashing/valid_palindrome.py
@@ -0,0 +1,15 @@
+"""
+Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+"""
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string_to_check = ''.join(char.lower() for char in s if char.isalnum())
+
+        if string_to_check == string_to_check[::-1]:
+            return True
+        return False
