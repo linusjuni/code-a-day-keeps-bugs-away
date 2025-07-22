391e1c8cefba828dabe98f2be8b78fa9249df29d
Linus Juni <linus@juni.dk>
2025-07-22 22:31:08 +0200

    Create longest_consecutive_sequence.py

---
+++ neetcode/arrays-and-hashing/longest_consecutive_sequence.py
@@ -0,0 +1,22 @@
+"""
+Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
+
+You must write an algorithm that runs in O(n) time.
+"""
+
+from typing import List
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        longest = 0
+
+        for n in num_set:
+            if n - 1 not in num_set:
+                length = 1
+                while n + length in num_set:
+                    length += 1
+                longest = max(longest, length)
+        return longest
