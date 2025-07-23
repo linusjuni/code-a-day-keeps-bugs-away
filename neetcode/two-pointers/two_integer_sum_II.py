5c67e586b9d3a2d1bfc88c7bea3fa08c18a1d0ef
Linus Juni <linus@juni.dk>
2025-07-23 18:45:14 +0200

    Create two_integer_sum_II.py

---
+++ neetcode/two-pointers/two_integer_sum_II.py
@@ -0,0 +1,24 @@
+"""
+Given an array of integers numbers that is sorted in non-decreasing order.
+
+Return the indices (1-indexed) of two numbers, [index1, index2], such that they add up to a given target number target and index1 < index2. Note that index1 and index2 cannot be equal, therefore you may not use the same element twice.
+
+There will always be exactly one valid solution.
+
+Your solution must use O(1) additional space.
+"""
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+
+        while True:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
